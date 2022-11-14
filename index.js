9c88aa314f8ca6516791cfca2a786516dd2de7a7
RomanQuryanov <RomanQuryanov@gmail.com>
2022-11-14 07:50:20 +0400

    0.0.0.5 Beta
    НУ ЖЕ

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const Discord = require("discord.js");
 const bot =  [ new Discord.Client(), new Discord.Client()];
-const config = require("./config.json")
 const delay = ms => new Promise(res => setTimeout(res, ms));
-const auth = [process.env.TOKEN1,process.env.TOKEN2,process.env.TOKEN3]
+const auth = [
+    "OTQ4MjU4NzQzODg2NDI2MTEy.GpxGpF.1_AoqyTuLwmOaa6fyMZjT3bCJWMWDMQl7qMNCI"
+    , "Nzc5Njk4NTY3Njg1NjAzMzM4.Gr3mIZ.xw4DnSG-W3JXhHeHm86VLFsX8vSQT6OuptL8fM"
+    ,"OTkxNjc5MzcwOTA3MzYxMzAw.GsAT3y.ASDWW6ug7nEXfTL_ACkPcQYmSh7qww2Om6QvEE"]
 
-const express = require("express")
-const app = express()
-
-const PORT = process.env.PORT || 80
-
+const sleep = async (milliseconds) => {
+await new Promise(resolve => {
+    return setTimeout(resolve,milliseconds);
+});
+};
 
 for (const token of auth) {
     const bot = new Discord.Client()
@@ -23,7 +25,7 @@ for (const token of auth) {
             message.channel.send('ae/attack');
         }, 3050);  
 	};
-    if (message.content.startsWith('Подождите') & message.author.id === '891713802096230461' ) {
+    if (message.content.startsWith('Подождите 0:59:5') & message.author.id === '891713802096230461' ) {
         setTimeout(() => {
             message.channel.send('ae/attack');
         }, 3600500);  
